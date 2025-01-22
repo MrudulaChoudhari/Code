0ef8547883699327259868ad7b367885ce4030d2
MrudulaChoudhari <choudharimrudula4@gmail.com>
2025-01-22 15:31:45 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { useCallback, useState } from 'react';
+import Navbar from './components/Navbar';
+import { javascript } from "@codemirror/lang-javascript";
+import{html} from"@codemirror/lang-html";
+import{css}from"@codemirror/lang-css";
+import CodeMirror from "@uiw/react-codemirror";
+import Result from './components/Result';
+
+    function App() {
+      //* userstates 
+      const [html_edit, setHtml_Edit]=useState(' ');
+      const[css_edit, setCss_Edit]=useState(' ');
+      const[js_edit, setJs_Edit]=useState(' ');
+
+
+//* Html onchange handler
+const onChangeHtml = useCallback((value)=>{
+ setHtml_Edit(value);
+}, [ ]);
+
+//* Css onchange handler
+const onChangeCss = useCallback((value)=>{
+  setCss_Edit(value);
+}, [ ]);
+
+//* JavaScript onchange handler
+const onChangeJavascript = useCallback((value)=>{
+  setJs_Edit(value);
+}, [ ]);
+
+//* Html Document
+const srcCode = `
+<html>
+  <body>${html_edit}</body>
+  <style>${css_edit}</style>
+  <script>${js_edit}</script>
+  </html>
+`
+
+      return (
+        <div >
+          <Navbar />
+          <div className=" p-2">
+               <div className=" grid grid-cols-1 lg:grid-cols-3 gap-2">
+                  <div className="bg-[#282c34] p-4 rounded-lg shadow">
+                    <h2 className="text-lg font-semibold mb-2 text-white">HTML</h2>
+                    <CodeMirror
+                    className="text-xl  border-gray-700 border"
+                    value={html_edit}
+                    height="342px"
+                    theme="dark"
+                    extensions={[html(true)]}
+                    onChange={onChangeHtml}
+                    />
+                  </div>
+                <div className="bg-[#282c34] p-4 rounded-lg shadow">
+                   <h2 className="text-lg font-semibold mb-2 text-white">CSS</h2>
+                   <CodeMirror
+                    className="text-xl border-gray-700 border"
+                    value={css_edit}
+                    height="342px"
+                    theme="dark"
+                    extensions={[css(true)]}
+                    onChange={onChangeCss}
+                    />
+                </div>
+                <div className="bg-[#282c34] p-4 rounded-lg shadow"  >
+  
+                  <h2 className="text-lg font-semibold mb-2 text-white">JavaScript</h2>
+                  <CodeMirror
+                    className="text-xl border-gray-700 border"
+                    value={js_edit}
+                    height="342px"
+                    theme="dark"
+                    extensions={[javascript(true)]}
+                    onChange={onChangeJavascript}
+                    />
+                </div>
+               </div>
+
+               <Result
+               srcCode={srcCode}
+               />
+          </div>
+        </div>
+      );
+    }
+    
+    export default App;
+    
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='main lg:flex flex-wrap justify-between items-center px-4 bg-[#2f3640] py-4'>
+
+        <div className="flex justify-end items-center text-white text-bold">
+        <h1 className='text-bold text-3xl text-white  '>Code Editor</h1>
+        
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
